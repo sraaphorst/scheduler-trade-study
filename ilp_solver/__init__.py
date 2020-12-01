d74bb0373fbfc981127acb76843645367afb3abf
Sebastian Raaphorst <sraaphorst@gmail.com>
2020-12-01 17:43:05 -0300

    SCHED-16: Added modeling to determine or set at what time slot observations are scheduled for ILP.

--- ilp_solver/__init__.py
+++ ilp_solver/__init__.py
@@ -2,11 +2,12 @@
 # By Sebastian Raaphorst, 2020.
 
 # Load Gurobi if possible, and otherwise fall back on CBC.
-try:
-    import gurobipy
-    from ilp_solver.gurobi_solver import *
-    # This will throw an exception if we do not have an active license.
-    m = Model()
-except gurobipy.GurobiError:
-    from ilp_solver.cbc_solver import *
-# from ilp_solver.cbc_solver import *
+# try:
+#     import gurobipy
+#     from ilp_solver.gurobi_solver import *
+#     # This will throw an exception if we do not have an active license.
+#     m = Model()
+# except gurobipy.GurobiError:
+#     from ilp_solver.cbc_solver import *
+
+from ilp_solver.cbc_solver import *
